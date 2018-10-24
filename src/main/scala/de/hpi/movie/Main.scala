d1ed3c011dfac70488589a9a3d105caec32b54a0
Lando-L <lando.loeper@gmx.net>
2018-10-24 12:36:40 +0200

    Minor refactorings

--- src/main/scala/de/hpi/movie/Main.scala
+++ src/main/scala/de/hpi/movie/Main.scala
@@ -1,12 +1,12 @@
 package de.hpi.movie
 
 import de.hpi.movie.core.Integration
-import de.hpi.movie.source.imdb.Imdb
+import de.hpi.movie.source.imdb.{Imdb, ImdbInstances}
 import org.apache.spark.sql.SparkSession
 
 object Main {
 	def main(args: Array[String]): Unit = {
-		import de.hpi.movie.source.imdb.ImdbInstances._
+		import ImdbInstances._
 
 		val spark = SparkSession
 			.builder()
@@ -14,9 +14,11 @@ object Main {
 			.config("spark.master", "local")
 			.getOrCreate()
 
-		Integration
+		val is = Integration
 			.setup[Imdb](Map("ratingPath" -> "../imdb/title.ratings-10.tsv", "basicPath" -> "../imdb/title.basics-10.tsv"))
 			.run(spark)
+
+		is.foreach(_.show())
 
 		/*KaggleWrapper
   		.load(spark)(Map("ratingPath" -> "../kaggle/ratings_small.csv", "basicPath" -> "../kaggle/movies_metadata.csv"))
--- src/main/scala/de/hpi/movie/core/Integration.scala
+++ src/main/scala/de/hpi/movie/core/Integration.scala
@@ -1,26 +1,23 @@
 package de.hpi.movie.core
 
 import cats.data.Reader
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.{Dataset, SparkSession}
 
 trait Integration[A] {
 	protected def normalize: Movie => Movie
 	//def cluster[A]: Movie => A
 	//def deduplicate: (Movie, Movie) => Double
 
-	def setup(config: Map[String, String])(implicit w: Wrapper[A]): Reader[SparkSession, Unit] =
+	def setup(config: Map[String, String])(implicit w: Wrapper[A]): Reader[SparkSession, Option[Dataset[Movie]]] =
 		Reader {
 			sparkSession =>
 				import sparkSession.implicits._
 
-				w
-					.load(config)(sparkSession)
-					.map(_.flatMap(w.asMovie).map(normalize).count())
-					.foreach(println)
+				w.load(config)(sparkSession).map(_.flatMap(w.asMovie).map(normalize))
 		}
 }
 
 object Integration {
-	def setup[A](config: Map[String, String])(implicit i: Integration[A], w: Wrapper[A]): Reader[SparkSession, Unit] =
+	def setup[A](config: Map[String, String])(implicit i: Integration[A], w: Wrapper[A]): Reader[SparkSession, Option[Dataset[Movie]]] =
 		i.setup(config)
 }
